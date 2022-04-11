491737e08477c074aa6e470a2efa7fd53ce5657d
MyeongHyeonst <80314715+MyeongHyeonst@users.noreply.github.com>
2022-04-12 02:58:18 +0900

    WashingPatch(pet 적용, ui 개선, gamemanager status 연동, Prefab 제작)

--- main/Assets/Scripts/WashingController.cs
+++ main/Assets/Scripts/WashingController.cs
@@ -6,28 +6,49 @@ using UnityEngine.UI;
 public class WashingController : MonoBehaviour
 {
     public GameObject bubble;
-    public GameObject pet;
-    SphereCollider rangeCollider;
+    private GameObject pet;
+    public GameObject clearUI;
+    CapsuleCollider rangeCollider;
 
     public Slider washingGauge;
-    // Start is called before the first frame update
+    private bool isClear;
+
     void Start()
     {
-        rangeCollider = pet.GetComponent<SphereCollider>();
-      //  washingGauge = GetComponent<Slider>();
+        clearUI.SetActive(false);
+        washingGauge.value = 0;
+        isClear = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Manage status
+        if (washingGauge.value >= 100 && !isClear)
+        {
+            clearUI.SetActive(true);
+            isClear = true;
+
+            Debug.Log("Stop");
+
+            GameManager.Instance.Cleanliness += 20;
+            GameManager.Instance.Love += 5;
+            GameManager.Instance.Fullness -= 10;
+
+            clearUI.SetActive(true);
+        }
+
+        // AR
         DetectHandGestureGrab();
         DetectMouseClick();
-        if (washingGauge.value == 100)
-        {
-            washingGauge.value = 0;
-        }
     }
 
+    private void FixedUpdate()
+    {
+        pet = GameObject.FindGameObjectWithTag("Player");
+        rangeCollider = pet.GetComponent<CapsuleCollider>();
+    }
+
+    // AR version
     void DetectHandGestureGrab()
     {
         //Information of the hand
@@ -36,23 +57,27 @@ public class WashingController : MonoBehaviour
         // When I perform the Grab Gesture
         if (detectedHand.gesture_info.mano_gesture_trigger == ManoGestureTrigger.GRAB_GESTURE)
         {
-            washingGauge.value += 10;
+            if(washingGauge.value < 100)
+               washingGauge.value += 10;
             RandomSpawn();
         }
 
     }
 
+    // Click version
     void DetectMouseClick()
     {
-        // When I perform the Grab Gesture
         if (Input.GetMouseButtonDown(0))
         {
             washingGauge.value += 10;
             RandomSpawn();
+            Debug.Log("게이지 : " + washingGauge.value);
+            Debug.Log("청결 : " + GameManager.Instance.Cleanliness);
         }
 
     }
 
+    // Bubble spawn
     private void RandomSpawn()
     {
         Vector3 petPos = pet.transform.position;
@@ -61,9 +86,10 @@ public class WashingController : MonoBehaviour
         float rangeY = rangeCollider.bounds.size.y;
         float rangeZ = rangeCollider.bounds.size.z;
 
-        rangeX = Random.Range((rangeX / 2) * -1, rangeX / 2);
-        rangeY = Random.Range((rangeY / 2) * -1, rangeY / 2);
-        rangeZ = Random.Range((rangeZ / 2) * -1, rangeZ / 2);
+        // Spawn range
+        rangeX = Random.Range((rangeX/2) * -1, rangeX/2 );
+        rangeY = Random.Range((rangeY/2) * -1, rangeY/2 );
+        rangeZ = Random.Range((rangeZ/2) * -1, rangeZ/2 );
 
         Vector3 randomPos = new Vector3(rangeX, rangeY, rangeZ);
         Vector3 spawnPos = petPos + randomPos;
