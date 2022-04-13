0f1faefbccaa176420fe04f8b13a44753c74c9c1
insu12021202 <insu12021202@gmail.com>
2022-04-13 19:32:16 +0900

    Plane prefab 변경, Pet이 AR Camera를 바라보게 항상 Rotate, Greeting 기능 구현, Pet Status GUI와 GameManager 변수 연동

--- main/Assets/Scripts/GameManager.cs
+++ main/Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
     public int Fullness = 50;
     public int[] foodStock = { 1, 1, 1, 1, 0 }; // 먹이 인벤토리 순서대로
     public bool isFoodSpawn = false;
+    public string petName = "Test Name";
 
     void Awake()
     {
