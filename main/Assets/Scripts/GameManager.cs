57ff3337db4aa5d303b67a8c6c743931854ba74d
ChoiDongHo <goguma1000@ajou.ac.kr>
2022-04-20 13:40:22 +0900

    GameManager 애정도감소 1->60초 Timer변수 추가. 각 상호작용 종료 시 GameManager.Instance.Timer = 0.0f로 초기화 해주시면 됩니다,

--- main/Assets/Scripts/GameManager.cs
+++ main/Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     public int[] petStomach = { 0, 0, 0, 0, 0, 0, 0 };
     public int petNum = 0;
     public bool isFoodSpawn = false;
-
+    public float Timer = 0.0f;
     [HideInInspector]
     public string petName = null;
 
@@ -64,7 +64,7 @@ public class GameManager : MonoBehaviour
         {
             if ((Cleanliness <= 20 || Fullness <= 20) && Love > 0)
             {
-                yield return new WaitForSeconds(1);
+                yield return new WaitForSeconds(60);
                 if (Love - 5 < 0) Love = 0;
                 else Love -= 5;
             }
